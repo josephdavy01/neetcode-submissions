0a03cb905d0aa13002702441746a67b387f0b63e
josephdavy01 <90496871+josephdavy01@users.noreply.github.com>
2026-08-29 19:58:56 +0530

    Add: maximum-number-of-balloons - submission-1

---
+++ Data Structures & Algorithms/maximum-number-of-balloons/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        counts = {'b': 0, 'a': 0, 'l': 0, 'o': 0, 'n': 0}
+
+        for char in text:
+            if char in counts:
+                counts[char]+=1
+
+        return min(counts['b'],counts['a'],counts['l']//2,counts['o']//2,counts['n'])    
+
+
+        
